c8dcc4738cd9a8864ef2b3b65b08e3da36ac54d3
shimoyama@a-good.co.jp <shimoyama@a-good.co.jp>
2017-10-11 18:05:43 +0900

    comm

---
+++ ReadLineSample.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ReadLineSample {
+
+	public static void main(String[] args) {
+
+		String data;
+
+		FileReader in = null;
+		BufferedReader buf = null;
+
+		try {
+			in = new FileReader("books.txt");
+
+			buf = new BufferedReader(in);
+
+			//BufferedReader buf = new BufferedReader(new FileReader("test.txt"));
+
+			while (true) {
+				data = buf.readLine();
+
+				if (data == null) {
+					break;
+				}
+				System.out.println(data);
+			}
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+
+		} finally {
+			try {
+				if (buf != null) {
+					buf.close();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+		}
+
+	}
+
+}
